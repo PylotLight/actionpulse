ac10a5e606dc9f4a1d427cc605d82778fef984cc
Light <lochois@gmail.com>
2026-03-03 18:27:09 +1100

    Update actionpulse.rb

--- Casks/actionpulse.rb
+++ Casks/actionpulse.rb
@@ -1,6 +1,6 @@
 cask "actionpulse" do
-  version "0.2.0"
-  sha256 "6875c85a27bcffccba584aa151cbd67a36569461c0a0390e61765dfc3cbdf5d1"
+  version "0.3.0"
+  sha256 "16297aedd47f0c28f0694fb03637f41c56f5c8ec713cb51eddd15d3a3d5fa6bb"
 
   url "https://github.com/PylotLight/homebrew-actionpulse/releases/download/v#{version}/actionpulse.macOS.zip"
   name "ActionPulse"
